071929d219cc89c257ff37b89d01d16090babc25
Edward4213 <166951468+Edward4213@users.noreply.github.com>
2026-09-11 12:56:49 +0100

    Update models.py
    Implemented black scholes put and call

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
 import math
+from scipy.stats import norm
+
+def d1(S,K,T,r,sigma):
+    d1 = (math.log(S/K) + (r + ((sigma**2)/2))*T)/(sigma*math.sqrt(T))
+    return d1
+
+def d2(S,K,T,r,sigma):
+    d2 = d1(S,K,T,r,sigma) - sigma*math.sqrt(T)
+    return d2
+
+def call_price(S,K,T,r,sigma):
+
+    d1_value = d1(S,K,T,r,sigma)
+    d2_value = d2(S,K,T,r,sigma)
+
+    call_price_value = S*(norm.cdf(d1_value)) - K*math.exp(-r*T)*norm.cdf(d2_value)
+    return call_price_value
+
+def put_price(S,K,T,r,sigma):
+
+    d1_value = d1(S,K,T,r,sigma)
+    d2_value = d2(S,K,T,r,sigma)
+
+    put_price_value = K*math.exp(-r*T)*norm.cdf(d2_value) - S*(norm.cdf(d1_value))
+    return put_price_value
