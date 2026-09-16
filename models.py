a6bca3397236897ebb238c0bb275666003adc5f5
Edward4213 <166951468+Edward4213@users.noreply.github.com>
2026-09-16 11:50:30 +0100

    Update models.py

--- models.py
+++ models.py
@@ -50,3 +50,32 @@ def vega(S,K,T,r,sigma):
 
     vega_value = S*math.sqrt(T)*N_of_d1
     return vega_value
+
+def call_theta(S,K,T,r,sigma):
+    d2_value = d2(S,K,T,r,sigma)
+    d1_value = d1(S,K,T,r,sigma)
+
+    phi_of_d1 = math.exp((((-(d1_value) ** 2)) / 2)) / math.sqrt(2 * math.pi)
+    call_theta_value = -((S*phi_of_d1*sigma)/(2*math.sqrt(T))) - r*K*math.exp(-r*T)*norm.cdf(d2_value)
+    return call_theta_value
+
+
+def put_theta(S,K,T,r,sigma):
+    d2_value = d2(S,K,T,r,sigma)
+    d1_value = d1(S,K,T,r,sigma)
+
+    phi_of_d1 = math.exp((((-(d1_value) ** 2)) / 2)) / math.sqrt(2 * math.pi)
+    put_theta_value = -((S*phi_of_d1*sigma)/(2*math.sqrt(T))) + r*K*math.exp(-r*T)*norm.cdf(-d2_value)
+    return put_theta_value
+
+
+def call_rho(S,K,T,r,sigma):
+    d2_value = d2(S,K,T,r,sigma)
+    call_rho_value = K*T*math.exp(-r*T)*norm.cdf(d2_value)
+    return call_rho_value
+
+
+def put_rho(S,K,T,r,sigma):
+    d2_value = d2(S,K,T,r,sigma)
+    put_rho_value = -K*T*math.exp(-r*T)*norm.cdf(-d2_value)
+    return put_rho_value
