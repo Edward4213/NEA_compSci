125161ac1fb8f53d91c7803200826e321a7945f8
Edward4213 <166951468+Edward4213@users.noreply.github.com>
2026-09-16 11:08:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from models import call_price, put_price
+from portfolio import portfolio, total_value, portfolio_weights
 
 S = 90000
 K = 82000
@@ -8,3 +9,5 @@ sigma = 0.50
 
 print(f"The call price is {call_price(S, K, T, r, sigma)}")
 print(f"The put price is {put_price(S, K, T, r, sigma)}")
+print("Total:", total_value(portfolio))
+print("Weights:", portfolio_weights(portfolio))
